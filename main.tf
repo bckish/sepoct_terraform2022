6e59d3a809f3456e8e70f93dcf78705ccc0ee979
Kishore Billu <107416276+bckish@users.noreply.github.com>
2022-09-24 22:32:40 +0200

    add s3 bucket

--- main.tf
+++ main.tf
@@ -8,3 +8,9 @@ resource "aws_instance" "example" {
   ami           = "ami-006d3995d3a6b963b"
   instance_type = "t2.micro"
 }
+
+# Create S3 bucket
+resource "aws_s3_bucket" "example" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts
+  bucket = "wezvatech-adam-demo-s3-sep22"
+}
